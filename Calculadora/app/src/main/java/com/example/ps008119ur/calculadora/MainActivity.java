ec68a1f1838f2ed8ac61c26affef7381d4662f8f
Paulo Henrique Nunes <paulohcode@gmail.com>
2018-09-17 15:48:10 -0300

    Adicionado a Função + - / *

--- Calculadora/app/src/main/java/com/example/ps008119ur/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/ps008119ur/calculadora/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ps008119ur.calculadora;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
     EditText ed1;
     EditText ed2;
     TextView tvA;
-    Button btSoma;
+    Button btnSoma;
+    Button btnSub;
+    Button btnDiv;
+    Button btnMult;
 
 
 
@@ -25,13 +28,17 @@ public class MainActivity extends AppCompatActivity {
         ed1 = findViewById(R.id.valor1);
         ed2 = findViewById(R.id.valor2);
         tvA = findViewById(R.id.textViewAviso);
-        btSoma = findViewById(R.id.Soma);
+        btnSoma = findViewById(R.id.btnSoma);
+        btnSub = findViewById(R.id.btnSub);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMult = findViewById(R.id.btnMult);
 
-        btSoma.setOnClickListener(new View.OnClickListener() {
+        //inicio da ação de soma
+        btnSoma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if (ed1.getText().toString().isEmpty() && ed2.getText().toString().isEmpty()){
+                if (ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
 
                     tvA.setText("Digite um valor");
                     return;
@@ -59,6 +66,123 @@ public class MainActivity extends AppCompatActivity {
 
 
             }
-        });
+        }); //fim da ação de soma
+
+        //inicio da ação de subtrair
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
+
+                    tvA.setText("Digite um valor");
+                    return;
+                }
+
+                Double v1;
+                Double v2;
+                Double res;
+
+                v1 = Double.parseDouble( ed1.getText().toString());
+                v2 = Double.parseDouble( ed2.getText().toString());
+
+                res = v1 - v2;
+
+
+                Intent ResultadoActivity = new Intent(MainActivity.this, ResultadoActivity.class);
+
+                Bundle parametros = new Bundle();
+
+                parametros.putString("resultado", res.toString() );
+
+                ResultadoActivity.putExtras(parametros);
+
+                startActivity(ResultadoActivity);
+
+
+            }
+        });//fim da ação de subtrair
+
+        //inicio da ação de Divisão
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()) {
+
+                    tvA.setText("Digite um valor");
+                    return;
+                }
+
+
+                Double v1;
+                Double v2;
+                Double res;
+
+                v1 = Double.parseDouble(ed1.getText().toString());
+                v2 = Double.parseDouble(ed2.getText().toString());
+
+                if (v1 == 0 || v2 == 0) {
+                    tvA.setText("Não e possivel divisão por zero");
+                    return;
+                } else {
+
+
+                    res = v1 * v2;
+
+
+                    Intent ResultadoActivity = new Intent(MainActivity.this, ResultadoActivity.class);
+
+                    Bundle parametros = new Bundle();
+
+                    parametros.putString("resultado", res.toString());
+
+                    ResultadoActivity.putExtras(parametros);
+
+                    startActivity(ResultadoActivity);
+
+
+                }
+            }
+
+        });//fim da ação de Divisão
+
+        //inicio da ação de Multiplicação
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
+
+                    tvA.setText("Digite um valor");
+                    return;
+                }
+
+                Double v1;
+                Double v2;
+                Double res;
+
+                v1 = Double.parseDouble( ed1.getText().toString());
+                v2 = Double.parseDouble( ed2.getText().toString());
+
+                res = v1 * v2;
+
+
+                Intent ResultadoActivity = new Intent(MainActivity.this, ResultadoActivity.class);
+
+                Bundle parametros = new Bundle();
+
+                parametros.putString("resultado", res.toString() );
+
+                ResultadoActivity.putExtras(parametros);
+
+                startActivity(ResultadoActivity);
+
+
+            }
+        });//fim da ação de multiplicação
+
+
+
     }
 }
--- Calculadora/app/src/main/java/com/example/ps008119ur/calculadora/ResultadoActivity.java
+++ Calculadora/app/src/main/java/com/example/ps008119ur/calculadora/ResultadoActivity.java
@@ -1,8 +1,8 @@
 package com.example.ps008119ur.calculadora;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class ResultadoActivity extends AppCompatActivity {
